a3143168670a80094ccdd1aae21bd877f1e2bed7
Samantha Colin <samantha.colin022@gmail.com>
2023-12-02 13:19:23 -0600

    bookmark

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -12,6 +12,7 @@ import './../assets/styles/base/typography.css';
 // Local CSS Styles //
 import styles from './../components/Recipe/Recipe.module.css';
 import './../components/Recipe/RecipeClicked.module.css';
+import './../assets/styles/layouts/grid.css';
 
 
 function HomePage() {
@@ -34,8 +35,16 @@ function HomePage() {
         <div className="gridContainer">
           {recipes.map((recipe) => (
             <div className={styles.recipeContainer} key={recipe.id} onClick={() => openRecipe(recipe)}>
-              <img src={recipe.image} alt={recipe.name} />
+              
+              <img src={require(`./../../assets/images/${recipe.image}`).default} alt={"finished " + recipe.name + " recipe"} />
+              LEFT OFF TRYING TO DYNAMICALLY RENDER IMAGES, BUT GOT AN ERROR. WAS IN THE MIDDLE OF ASKING CHATGBT
+
+
               <h2>{recipe.name}</h2>
+              <h3>Cook Time</h3> {/* Style these with flexbox to make two-column layout */}
+              <p>{recipe.cookTime}</p>
+              <h3>Prep Time</h3>
+              <p>{recipe.prepTime}</p>
               {/* Rest of Recipe Info */}
             </div>
           ))}
